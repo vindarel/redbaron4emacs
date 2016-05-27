28f73ebc6143b60c13bafc371b57a066057f61b8
vindarel <ehvince@mailz.org>
2016-05-27 02:21:24 +0200

    consistent 'which-function' with only the method name
    the one from which-func-mode outputs "name (def)",
    the one from python.el outputs classname.classname...upmethod.method

--- red4emacs.el
+++ red4emacs.el
@@ -7,7 +7,9 @@
   (let* ((args-list (-concat (my-py-get-function-args) (list arg)))
         (def (concat
               "def "
-              (which-function) ;; warning: can output "name (def)"
+              ;; (which-function) ;; inconsistent with "name (def)"
+              ;; (python-info-current-defun) ;; no: includes class names
+              (my-python-info-current-defun)
               "("
               (mapconcat #'identity
                          args-list
@@ -15,3 +17,68 @@
               "): pass")))
     (insert (shell-command-to-string (concat "python " red4emacs-path " '" def "'")))
   ))
+
+(defun my-python-info-current-defun (&optional include-type)
+  "Return name of surrounding function with Python compatible dotty syntax.
+Optional argument INCLUDE-TYPE indicates to include the type of the defun.
+This function is compatible to be used as
+`add-log-current-defun-function' since it returns nil if point is
+not inside a defun."
+  (save-restriction
+    (widen)
+    (save-excursion
+      (end-of-line 1)
+      (let ((names)
+            (starting-indentation (current-indentation))
+            (starting-pos (point))
+            (first-run t)
+            (last-indent)
+            (type))
+        (catch 'exit
+          ;; (while
+              (python-nav-beginning-of-defun 1)
+            (when (save-match-data
+                    (and
+                     (or (not last-indent)
+                         (< (current-indentation) last-indent))
+                     (or
+                      (and first-run
+                           (save-excursion
+                             ;; If this is the first run, we may add
+                             ;; the current defun at point.
+                             (setq first-run nil)
+                             (goto-char starting-pos)
+                             (python-nav-beginning-of-statement)
+                             (beginning-of-line 1)
+                             (looking-at-p
+                              python-nav-beginning-of-defun-regexp)))
+                      (< starting-pos
+                         (save-excursion
+                           (let ((min-indent
+                                  (+ (current-indentation)
+                                     python-indent-offset)))
+                             (if (< starting-indentation  min-indent)
+                                 ;; If the starting indentation is not
+                                 ;; within the min defun indent make the
+                                 ;; check fail.
+                                 starting-pos
+                               ;; Else go to the end of defun and add
+                               ;; up the current indentation to the
+                               ;; ending position.
+                               (python-nav-end-of-defun)
+                               (+ (point)
+                                  (if (>= (current-indentation) min-indent)
+                                      (1+ (current-indentation))
+                                    0)))))))))
+              (save-match-data (setq last-indent (current-indentation)))
+              (if (or (not include-type) type)
+                  (setq names (cons (match-string-no-properties 1) names))
+                (let ((match (split-string (match-string-no-properties 0))))
+                  (setq type (car match))
+                  (setq names (cons (cadr match) names)))))
+            ;; Stop searching ASAP.
+            (and (= (current-indentation) 0) (throw 'exit t)))
+        ;; )
+        (and names
+             (concat (and type (format "%s " type))
+                     (mapconcat 'identity names ".")))))))
